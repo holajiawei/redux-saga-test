aeec7448cd572d4a13997f8292fa1c1b24b494f7
java <java@heyjiawei.club>
2017-09-16 22:32:22 +0800

    add demo

---
+++ index.js
@@ -0,0 +1,14 @@
+import { race, take, put } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+
+function* fetchPostsWithTimeout() {
+  const {posts, timeout} = yield race({
+    posts: call(fetchApi, '/posts'),
+    timeout: call(delay, 1000)
+  })
+
+  if (posts)
+    put({type: 'POSTS_RECEIVED', posts})
+  else
+    put({type: 'TIMEOUT_ERROR'})
+}
